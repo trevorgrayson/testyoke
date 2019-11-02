30389d78e41c7618c8bb7dc0d2f9446861e8c171
trevor grayson <trevor@trevorgrayson.com>
2019-11-01 17:52:48 -0700

    WIP: setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='testyoke',
+    version='0.0.2',
+    author='trevor grayson',
+    author_email='trevor@ipsumllc.com',
+    description='Provide insights on the outcomes of software test cases.',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/trevorgrayson/testyoke',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
